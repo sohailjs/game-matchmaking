fadc6fd0cedc6eab7fcfdaeabfe278730a5c9114
sohail <sohail@supergaming.com>
2024-01-18 12:35:59 +0530

    simple matchmaking

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+func main() {
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	for i := 0; i < 10; i++ {
+		addPlayer(redisClient, "P"+strconv.Itoa(i))
+	}
+
+}
+
+func addPlayer(redisClient *redis.Client, playerId string) {
+	err := redisClient.LPush(ctx, "matchmaking_pool", playerId).Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
